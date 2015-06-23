fb6fe5d319f292e9179c127ee1cd6a94e6367f8b
Andy Sonnenberger <sonne058@umn.edu>
2015-06-23 09:53:23 -0400

    added two functions

--- src/temperature.py
+++ src/temperature.py
@@ -7,6 +7,11 @@ def f_to_k(temp):
     converted = ((temp-32.0)*(5.0/9.0)) + 273.15
     return converted
 
+def k_to_c(temp)
+    return temp-273.15
+
+def f_to_c(temp)
+    return k_to_c(f_to_k(temp))
 
 print(f_to_k(212))
 print(f_to_k(32))
